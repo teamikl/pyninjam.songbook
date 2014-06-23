2cab93793b9ff68daae05ca929f3c38133e650d4
tea <Ikkei.Shimomura@gmail.com>
2014-06-23 16:02:00 +0900

    Fix missing .h in zip and no need .bat anymore

--- share/dev/tool/settings.py
+++ share/dev/tool/settings.py
@@ -1,5 +1,5 @@
 
-SRC_EXTENSIONS = ('.html', '.hta', '.txt', '.js', '.css', '.c', '.bat', '.py')
+SRC_EXTENSIONS = ('.html', '.hta', '.txt', '.js', '.css', '.c', '.h', '.py')
 BIN_EXTENSIONS = ('.ico', '.exe') # + ('.png', '.jpg', '.gif', '.bmp')
 ALL_EXTENSIONS = SRC_EXTENSIONS + BIN_EXTENSIONS
 IGNORE_PATH = '.hg'
